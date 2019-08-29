16dc64bd98a30a3d34e0b6f1cf568abcdb8c39fd
Woron <woron113@protonmail.com>
2019-08-29 22:49:16 +0200

    Documentation patch

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Bare-Bone-Affix
 Bare bone javascript affix plugin
+
+Description:
+    Bare bone affix is an affix plugin that support multiple affixes at the same time.
+    It has html scraping that also support multiple affixes at the same time.
+    Natively its has 3 built in positions (top, mid, bottom) but you can specify it by number.
+    It support jumping to the container, so basically you can set the affix element anywhere it dont has
+    to be the affix container.
+        note: You can use css @keyframes to make a smooth animation for the jump
+
+Scraping from html:
+    Affix: should get the class = 'affix-sja'
+    Affix container: should get the class = 'affix-container-sja'
+
+    You simply need to give the affix container relative element to affix, and the affix element
+    witch will be moved relative to the affix container.
+
+Classes:
+    The affix element gets an 'affix-mode' class when its affixed
+
+Only works on Y axis
--- bare-bone-affix.js
+++ bare-bone-affix.js
@@ -1,31 +1,46 @@
 /*
     Bare Bone affix
 
-     You simply need to give the affix container relative element to affix, and the affix element
-     witch will be moved relative to the affix container.
-     Affix should get the class = 'affix-sja'
-     Affix container should get the class = 'affix-container-sja'
+    Description:
+        Bare bone affix is an affix plugin that support multiple affixes at the same time.
+        It has html scraping that also support multiple affixes at the same time.
+        Natively its has 3 built in positions (top, mid, bottom) but you can specify it by number.
+        It support jumping to the container, so basically you can set the affix element anywhere it dont has
+        to be the affix container.
+            note: You can use css @keyframes to make a smooth animation for the jump
 
-     Written in ECMAScript 2015 javascript therefor it should be converted to pre version for browser support
-     as of now.
-     Only work on Y offset
+    Scraping from html:
+        Affix: should get the class = 'affix-sja'
+        Affix container: should get the class = 'affix-container-sja'
+
+        You simply need to give the affix container relative element to affix, and the affix element
+        witch will be moved relative to the affix container.
+
+    Classes:
+        The affix element gets an 'affix-mode' class when its affixed
+
+    Written in ECMAScript 2015 javascript therefor it should be converted to pre version for browser support
+    as of now.
+
+    Only works on Y axis
+
+    Github: https://github.com/woron113/Bare-Bone-Affix
 */
 
 class BareBoneAffix {
 
     /**
-     *
      * @param settings
      * @param affix (optional)
      * @param affixContainer (optional)
      */
     constructor(settings, affix, affixContainer) {
-
-        this.affix = affix;
         this.affixContainer = affixContainer;
+        this.affix = affix;
 
         // Sets the default settings
         this.setDefaultSettings(settings);
+
         // Inits methods
         this.init();
     }
@@ -37,13 +52,17 @@ class BareBoneAffix {
     setDefaultSettings(settings) {
 
         const defaultSettings = {
+            // If you want to init a build when the class instance is made (note: you need to give the class parameters)
             initBuild: false,
+            // If you want a plugin to scrape the html
             scraping: true,
+            // The position where the affix sits, it can be defaults: (top, mid, bottom) or any given number
             position: 'top',
-            jumpToOriginalPosition: true
+            // If the affix element is not in the container, and you want it to jump back to is original position set it to true
+            jumpToOriginalPosition: false
         };
 
-        this.settings = Object.assign(defaultSettings, settings);
+        this.settings = { ...defaultSettings, ...settings};
     }
 
     init() {
@@ -72,8 +91,6 @@ class BareBoneAffix {
             return 1;
         }
 
-
-
         for (let affix of affixs) {
             for (let key of affixContainers) {
 
@@ -88,8 +105,6 @@ class BareBoneAffix {
 
     /**
      * Building process which can be called manually
-     * @param affix element, native js
-     * @param affixContainer element, native js
      */
     build(affix, affixContainer) {
 
@@ -97,9 +112,14 @@ class BareBoneAffix {
             throw new Error('You need to give both affix and affixContainer when you try to build an affix!');
         }
 
+        /**
+         * affixPosition: the position where the affix sit in fixed mode relative to the windowTop
+         * triggerLine: the line where the affix mode is triggered relative to the windowTop
+         */
         let affixPosition = null,
         triggerLine = null;
 
+        // Setting the affix relativity datas
         switch(this.settings.position) {
             case (typeof this.settings.position === 'number'):
                 affixPosition = this.settings.position;
@@ -111,7 +131,7 @@ class BareBoneAffix {
                 triggerLine = window.innerHeight - affix.offsetHeight;
                 break;
 
-            case 'middle':
+            case 'mid':
                 affixPosition = window.innerHeight / 2 - affix.offsetHeight;
                 triggerLine = window.innerHeight / 2 - affix.offsetHeight;
                 break;
@@ -121,15 +141,19 @@ class BareBoneAffix {
                 triggerLine = 0;
         }
 
+        // Starting listening for change
         window.addEventListener('scroll', () => {
             const windowTop = (window.pageYOffset || document.scrollTop) - (document.clientTop || 0),
             affixContainerTop = affixContainer.offsetTop,
             affixContainerHeight = affixContainer.offsetHeight;
 
             if (windowTop > affixContainerTop - triggerLine && windowTop + triggerLine < (affixContainerTop + affixContainerHeight - affix.offsetHeight)) {
-                affix.classList.add('affix-mode');
-                affix.style.position = 'fixed';
-                affix.style.top = affixPosition;
+
+                if (affix.style.position !== 'fixed') {
+                    affix.classList.add('affix-mode');
+                    affix.style.position = 'fixed';
+                    affix.style.top = affixPosition;
+                }
 
             } else {
 
@@ -149,11 +173,3 @@ class BareBoneAffix {
         });
     }
 }
-
-const affix1 = new BareBoneAffix;
-
-document.getElementsByTagName('body').addEventListener('keypress', ev => {
-    const target = ev.target;
-
-    target.html(target.html() + ' ');
-});
